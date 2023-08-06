3995f126b9cec36ee998b5821d4371276b6aa84a
Rust Bytes <rustbytes@proton.me>
2023-08-06 20:54:51 +0100

    Setup build files

---
+++ build.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.hpp");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.hpp")
+        .clang_args(vec!["-x", "c++"])
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
---
+++ wrapper.hpp
@@ -0,0 +1,1 @@
+#include "./src/cpp/include/lib.hpp"

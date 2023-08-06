8b785b2ebbf808d3c138712700e30df05c556b3a
Rust Bytes <rustbytes@proton.me>
2023-08-06 21:57:03 +0100

    Move tests out of module
    This is because the `include` macro would need to be called twice, once for exposing the FFI and another time for the tests.
    
    Removing the module made it easier as the bindings would only need to be exposed once.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,19 @@
-mod tests {
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-    #[test]
-    fn default() {
-        assert_eq!(0, unsafe { IntegerWrapper::default().Get() });
-    }
+#[test]
+fn default() {
+    assert_eq!(0, unsafe { IntegerWrapper::default().Get() });
+}
 
-    #[test]
-    fn set_and_get() {
-        let mut int = IntegerWrapper::default();
+#[test]
+fn set_and_get() {
+    let mut int = IntegerWrapper::default();
 
-        unsafe { int.Set(10) };
+    unsafe { int.Set(10) };
 
-        assert_eq!(10, unsafe { int.Get() });
-    }
+    assert_eq!(10, unsafe { int.Get() });
 }

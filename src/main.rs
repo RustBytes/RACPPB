0208431801883b8775fcb99386e188b684ccf53f
Rust Bytes <rustbytes@proton.me>
2023-08-06 21:08:52 +0100

    Add some basic code for testing the library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 fn main() {
-    println!("Hello, world!");
+    let mut integer = IntegerWrapper::default();
+
+    let inner = unsafe { integer.Get() };
+
+    println!("{inner}");
 }
